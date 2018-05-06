3a15589e7c8500837cbbfd1f41a24a9456f2009c
S.Hossein Hosseini <seyedhosein88@yahoo.com>
2018-05-06 15:33:12 -0700

    clean route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 Route::get('/','HomeController@home')->name('home');
 
 Route::get('/about-me', 'HomeController@about_me')->name('about-me');
@@ -11,16 +10,5 @@ Route::get('/contact-me', 'HomeController@contact_me_view')->name('contact-me');
 Route::post('contact-me','HomeController@contact_action')->name('form-action');
 
 Route::get('/blog','PostController@index')->name('post-index');
-
-
-Route::get('/test', function (){
-   return view('welcome');
-});
-
-//
-//Route::get('/user/{id}', function ($id) {
-//
-//})->where(['id' => '[0-9]']);
-//
 
 

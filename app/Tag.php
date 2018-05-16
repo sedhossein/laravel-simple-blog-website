0b0db3554d155197413178c6123201d211cc361d
S.Hossein Hosseini <seyedhosein88@yahoo.com>
2018-05-16 04:26:14 -0700

    tag model created and edited

--- app/Tag.php
+++ app/Tag.php
@@ -8,6 +8,7 @@ class Tag extends Model
 {
     public function posts()
     {
-        return $this->belongsToMany(Post::class,'post_tag','tag_id','post_id');
+        return $this->belongsToMany(Post::class,
+            'post_tag','tag_id','post_id');
     }
 }

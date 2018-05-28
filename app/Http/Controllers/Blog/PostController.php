a00dabb41f6752740e2cff94aa003f476cd54ff1
S.Hossein Hosseini <seyedhosein88@yahoo.com>
2018-05-28 00:42:52 -0700

    Update PostController.php

--- app/Http/Controllers/Blog/PostController.php
+++ app/Http/Controllers/Blog/PostController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    
+    public function __construct()
+    {
+    //    $this->middleware('auth');
+    }
+    
     public function index()
     {
 
